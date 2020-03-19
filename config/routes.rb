000c1b7941c71c062ba1d08f894a7166d64403bf
Kevin Chavanne <kevin.chavanne@gmail.com>
2020-03-19 21:55:20 +0100

    Add some styles + pg + simple form + /join route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :rooms, only: [:create, :show], param: :token
+  resources :rooms, only: [:create, :show], param: :token do
+    member do
+      get '/join', to: 'rooms#join'
+    end
+  end
 
   root to: 'pages#home'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_03_19_191328) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "rooms", force: :cascade do |t|
     t.string "token", null: false
     t.datetime "created_at", precision: 6, null: false
